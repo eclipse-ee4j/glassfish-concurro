61667311363b987e6b5bc7ee0bd0f34aecb75daa
Arjan Tijms <arjan.tijms@omnifish.ee>
2025-12-16 16:45:49 +0100

    Start GlassFish Concurro 3.2
    This version will target Jakarta Concurrency 3.2/EE 12.
    
    Do away with the JDK 17 multi-release jar, and focus on JDK 21+

--- src/main/java/org/glassfish/concurro/virtualthreads/VirtualThreadsManagedThreadFactory.java
+++ src/main/java/org/glassfish/concurro/virtualthreads/VirtualThreadsManagedThreadFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2023, 2025 Contributors to the Eclipse Foundation.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -15,46 +15,106 @@
  */
 package org.glassfish.concurro.virtualthreads;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.glassfish.concurro.ContextServiceImpl;
 import org.glassfish.concurro.ManagedThreadFactoryImpl;
 import org.glassfish.concurro.internal.ManagedFutureTask;
+import org.glassfish.concurro.internal.ThreadExpiredException;
 import org.glassfish.concurro.spi.ContextHandle;
 
+import static java.lang.System.Logger.Level.ERROR;
+import static java.lang.System.Logger.Level.INFO;
+
 /**
- * Dummy Implementation of ManagedThreadFactory using Virtual Threads.
- * @author Kalin Chan
+ *
+ * @author Ondro Mihalyi
  */
 public class VirtualThreadsManagedThreadFactory extends ManagedThreadFactoryImpl {
+    // map from thread to the time of task start
+    // TODO replace ManagedThreadFactoryImpl.threads with startTimes.getKeySet?
+    final Map<Thread, Long> startTimes = new ConcurrentHashMap<>();
 
     public VirtualThreadsManagedThreadFactory(String name) {
-        super(null);
-        throw new UnsupportedOperationException("This feature is only supported for Java 21+");
+        super(name);
     }
 
     public VirtualThreadsManagedThreadFactory(String name, ContextServiceImpl contextService) {
-        super(null, null);
-        throw new UnsupportedOperationException("This feature is only supported for Java 21+");
-
+        super(name, contextService);
     }
 
     @Override
     protected Thread createThread(Runnable taskToRun, ContextHandle contextHandleForSetup) {
-        throw new UnsupportedOperationException("This feature is only supported for Java 21+");
+        RunnableWithContext taskToRunWithContext = new RunnableWithContext(taskToRun, contextHandleForSetup);
+        return Thread.ofVirtual().unstarted(taskToRunWithContext);
     }
 
     @Override
     protected void shutdown(Thread t) {
-        throw new UnsupportedOperationException("This feature is only supported for Java 21+");
+        if (t != null) {
+            /* TODO - interrup not guaranteed to work on all JDKs if the thread hasn't started yet.
+               Look how to improve this later. */
+            t.interrupt();
+        }
+
     }
 
     @Override
     public void taskStarting(Thread t, ManagedFutureTask task) {
-        throw new UnsupportedOperationException("This feature is only supported for Java 21+");
-
+        if (t != null) {
+            startTimes.put(t, System.currentTimeMillis()); // TODO: use nanoTime instead?
+        }
     }
 
     @Override
     public void taskDone(Thread t) {
-        throw new UnsupportedOperationException("This feature is only supported for Java 21+");
+        if (t != null) {
+            startTimes.remove(t);
+        }
     }
+
+    public boolean isTaskHung(Thread thread, long now) {
+        Long startTime = startTimes.get(thread);
+        if (startTime == null) {
+            return false;
+        }
+        return now - startTime > getHungTaskThreshold();
+    }
+
+    static private final System.Logger loggerForRunnableWithContext = System.getLogger(RunnableWithContext.class.getName());
+
+    private final class RunnableWithContext implements Runnable {
+
+        private final Runnable nestedRunnable;
+        private final ContextHandle contextHandleForSetup;
+        private final System.Logger logger = loggerForRunnableWithContext;
+
+        public RunnableWithContext(Runnable nestedRunnable, ContextHandle contextHandleForSetup) {
+            this.nestedRunnable = nestedRunnable;
+            this.contextHandleForSetup = contextHandleForSetup;
+        }
+
+        @Override
+        public void run() {
+            ContextHandle handle = null;
+            try {
+                if (contextHandleForSetup != null) {
+                    handle = getContextSetupProvider().setup(contextHandleForSetup);
+                }
+                nestedRunnable.run();
+            } catch (ThreadExpiredException ex) {
+                logger.log(INFO, ex.toString());
+            } catch (Throwable t) {
+                logger.log(ERROR, getName(), t);
+            } finally {
+                if (handle != null) {
+                    getContextSetupProvider().reset(handle);
+                }
+                removeThread(Thread.currentThread());
+            }
+        }
+
+    }
+
 }
